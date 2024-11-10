9c9a55f3e2fa64901be5ddd679c9f1aee64fbd3e
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-10 23:10:45 +0300

    Added a  relationship_app/models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 class Author(models.Model):
     name = models.CharField(max_length=130)
 
@@ -14,4 +14,5 @@ class Library(models.Model):
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    library= models.OneToOneField(Library, on_delete=models.CASCADE)
+
