33a329efb0cadd18b175736f542ad490b98ae147
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-15 22:12:03 +0300

    Added  ../LibraryProject/views.py

---
+++ django-models/LibraryProject/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Book
+
+#Create Viws here
+#Function-based view
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
