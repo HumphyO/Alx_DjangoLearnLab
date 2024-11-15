773b5f24d2089349900a79ae4bb7cc8327cd4eca
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-15 21:45:46 +0300

    Added a relationship_app/

---
+++ django-models/relationship_app/relationship_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from .models import Book
+
+
+# Create your views here.
+#Function_based view 
+def list_books(request):
+    books = Book.objects.all() #Retrieve all books from datbase
+    book_list = []
+    for book in books:
+        book_list.append(f"{book.title} by {book.author.name}")
+    return render(request, "relationship_app/list_books.html", {'books': books})
+
