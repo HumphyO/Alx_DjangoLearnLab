dea9a32c51c656f49a2da961f67cf02f4557d680
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-17 00:43:52 +0300

    Moidified the urls.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,8 +12,8 @@ urlpatterns =[
     path('admin/', views.admin_view, name = 'admin_view'),
     path('librarian/', views.librarian_view, name = 'librarian_view'),
     path('member/', views.member_view, name = 'member'),
-    path('create/', views.add_book, name = 'add_book'),
-    path('update/', views.book_update, name = 'book_update'),
-    path('delete/', views.book_delete, name = 'book_delete'),
+    path('add_book/', views.add_book, name = 'add_book'),
+    path('edit_book/', views.edit_book, name = 'edit_book'),
+    path('delete_book/', views.delete_book, name = 'delete_book'),
     ]
     
