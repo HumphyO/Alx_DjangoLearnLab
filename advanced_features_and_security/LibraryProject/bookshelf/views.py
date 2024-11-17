65443047dbd203cec8e2aaf9a2cd88b62a63d926
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-17 18:51:09 +0300

    Modified the views.py file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -6,9 +6,9 @@ from django.contrib.auth.decorators import permission_required, login_required
 @login_required
 def list_books(request):
     books = Book.objects.all()
-
+    return render(request, 'relationship_app/list_books.html', {'book': books})
 def raise_exception(request):
     
    raise Exception("This book is not in the library")  
 
-   return render(request, 'relationship_app/list_books.html', {'book': books})
+   
