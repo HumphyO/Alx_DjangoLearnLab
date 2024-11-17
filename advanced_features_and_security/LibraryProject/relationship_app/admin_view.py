3f02f05d79c561eb5043604d273660a9fba141d3
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-17 22:05:00 +0300

    Modified the admin_view.py file

--- advanced_features_and_security/LibraryProject/relationship_app/admin_view.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin_view.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-def is_admin(user):
-    return user.is_authenticated and user.profile_role == 'Admin'
+def user_is_admin(user):
+    return user.profile_role == 'Admin'
 
 @user_passes_test(is_admin)
 #Admin_view
 def admin_view(request):
-    context = {
-        'role': 'Admin',
-    }
-    return render(request, 'relationship/admin_view.html', {'role': 'Admin'})
+
+    return render(request, 'relationship_app/admin_view.html')
+
+admin_view = user_passes_test(user_is_admin)(admin_view)
