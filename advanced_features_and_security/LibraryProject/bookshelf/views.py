c46a5b1940be950ed6503a3ff99369ef1b54d8c3
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-17 19:33:38 +0300

    Modified the views.py file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from django.contrib.auth.decorators import permission_required, login_required
+from forms import ExampleForm
 
 # Create your views here.
 @login_required
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/book_list.html', {'book': books})
+
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            return redirect('book_list') # Redirects to book list after saving
+        else:
+            form = BookForm # Create a blank form
+
+        return render(request, 'bookshelf/book_form.html', {'form': form})
+
 def raise_exception(request):
     
    raise Exception("This book is not in the library")  
 
-   
