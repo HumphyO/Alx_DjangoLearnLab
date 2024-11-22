16ff01256c64a601f8d249e77d5ba7981108308a
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-22 16:54:45 +0300

    Added authentication and permission classes to BookList and BookViewSet respectively

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,11 +2,22 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+
+
 # Create your views here.
 class BookList (ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [ObtainAuthToken]
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [ObtainAuthToken]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+
