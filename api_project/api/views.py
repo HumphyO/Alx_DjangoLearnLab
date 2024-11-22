27aa05970017de87d28ae1a8e73776cbe75b52de
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-22 17:01:02 +0300

    Modified the BookList to include generics.ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
@@ -8,7 +9,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 # Create your views here.
-class BookList (ListAPIView):
+class BookList (generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     authentication_classes = [ObtainAuthToken]
