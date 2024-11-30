41ef0ee14e3b9e985fd0f7d2af2644ffe92dbd7d
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-30 21:09:50 +0300

    Created a custom views and configured it to handle CRUD operations

---
+++ advanced_api_project/api/views.py
@@ -0,0 +1,35 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from .serializers import BookSerializer
+from .models import Book
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+# Create your views here.
+# Retrieving all books Read_only accessible to everyone
+class BookListView(ListAPIView):
+   queryset= Book.objects.all()
+   serializer_class = BookSerializer
+   permission_classes = [AllowAny]
+
+# Retrieving a single book ID Read_only accessible to everyone
+class BookDetailView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny] 
+
+# Adding a new book for authenticated users only
+class BookCreateView(CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes= [IsAuthenticated]
+
+# Modifying an existing book for authenticated users only
+class BookUpdateView(UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+# Removing a book for authenticated users only
+class BookDeleteView(DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
