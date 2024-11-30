33eb5fe25a4040d819bef91f2cf01944a3df7be2
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-30 23:17:53 +0300

    Added query capabilities to views.py

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView,
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Create your views here.
 # Retrieving all books Read_only accessible to everyone
@@ -9,6 +11,11 @@ class BookListView(ListAPIView):
    queryset= Book.objects.all()
    serializer_class = BookSerializer
    permission_classes = [AllowAny]
+   filter_backends = [DjangoFilterBackend, SearchFilter ]
+   filterset_fields = ['title', 'author', 'publication_date']
+   search_fields = ['title', 'author']
+   ordering_fields = ['title', 'publication_year']
+   ordering = ['title']
 
 # Retrieving a single book ID Read_only accessible to everyone
 class BookDetailView(RetrieveAPIView):
