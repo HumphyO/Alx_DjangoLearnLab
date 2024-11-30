518e93dbbc8618ec15ad22c3e3680ee3ccdf50ae
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-11-30 21:36:07 +0300

    Modified the views.py

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,18 +1,20 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from .serializers import BookSerializer
 from .models import Book
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
 # Retrieving all books Read_only accessible to everyone
 class BookListView(ListAPIView):
    queryset= Book.objects.all()
    serializer_class = BookSerializer
+   permission_classes = [AllowAny]
 
 # Retrieving a single book ID Read_only accessible to everyone
 class BookDetailView(RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [AllowAny] 
 
 # Adding a new book for authenticated users only
 class BookCreateView(CreateAPIView):
