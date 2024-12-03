74c7a0a9573e4575b3dd27c079928dd48be279cc
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-03 15:28:10 +0300

    Deleted a directory advanced_api_project

--- advanced_api_project/advanced_api_project/SECRET_KEY.py
+++
@@ -1,5 +0,0 @@
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y8f^+spx-eslvi_9x8274o19fiob^tum5e0fd)5vf77z86$7pk'
