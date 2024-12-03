ec09765659bcdf726c0157d67cc0682e8ec7a71c
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-03 16:15:44 +0300

    Modified the settings.py

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'advanced_api_project',
 ]
 
 MIDDLEWARE = [
