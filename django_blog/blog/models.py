3dc9f10fee0fe040fc11f67b6ff44c1d00f5bd83
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-06 01:25:21 +0300

    Modified thhe models.py to include User class

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class User(models.Model):
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    bio = models.TextField(max_length=270, blank=True)
     
+
 
     
