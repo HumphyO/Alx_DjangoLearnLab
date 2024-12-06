5f719a789f24a5ef647b1793af98c5bb5aaa567a
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-06 17:54:02 +0300

    Modified the views.py to include user authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -47,3 +50,44 @@ def profile_view(request):
         user.save()
         return redirect('profile')
     return render(request, 'profile.html')
+
+# list View to display all blog posts
+def PostListView(Listview):
+    model = Post
+    template_name = 'blog/blog_list.html'
+
+# Detail View to show individual blog posts
+def PostDetailView(DetailView):
+    model = Post
+    pk_url_kwarg = 'pk'
+    template_name = 'blog/blog_detail.html'
+
+# Create View to allow authenticated users to create new posts
+def PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/blog_create.html'
+
+# Update View to enable authors to edit their posts
+def PostUpdateView(LoginRequiredMixins, UpdateView):
+    model = Post
+    pk_url_kwarg = 'pk'
+    template_name = 'blog/blog_edit_form.html'
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+    
+# Delete View to allow authors delete their posts
+def PostDeleteView(DeleteView):
+    model = Post
+    pk_url_kwarg = 'pk'
+    template_name = 'blog/blog_delete_confirm.html'
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+
+
+
+
+
