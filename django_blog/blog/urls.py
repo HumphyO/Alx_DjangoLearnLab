034264b38b7ac6b5a4148ecab7dffd5cf2104c18
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-06 17:52:23 +0300

    Modified the urls to map CRUD views in urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,16 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = {
-    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
+    path('login/', views.LoginView.as_view(template_name = 'login.html'), name = 'login'),
+    path('logout/', views.LogoutView.as_view(), name = 'logout'),
     path('register/', views.register, name = 'register'),
     path('profile/', views.proofile_view, name = 'profile'),
+    path('/posts/', views.PostListView.as_view(), name = 'post_list'),
+    path('post/new/', views.PostCreateView.as_view(), name = 'post_create'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name = 'post_detail'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name = 'post_edit'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post_delete')
+
 }
