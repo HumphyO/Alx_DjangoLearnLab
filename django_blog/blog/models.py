7bdfe1db0a54d171f8df96dd16ab1f83b98ae7d6
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-06 23:05:06 +0300

    Modified the file to include comments model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -12,7 +13,16 @@ class Post(models.Model):
 class User(models.Model):
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
     bio = models.TextField(max_length=270, blank=True)
-    
+
+
+# Comment model for users to read and authenticated users to post edit and delete comments
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
 
 
     
