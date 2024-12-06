ac89071dedb42bbb3f148618d3203776741a6246
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-06 23:22:28 +0300

    Modified the urls for additional paths on comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,11 +6,13 @@ urlpatterns = {
     path('login/', views.LoginView.as_view(template_name = 'login.html'), name = 'login'),
     path('logout/', views.LogoutView.as_view(), name = 'logout'),
     path('register/', views.register, name = 'register'),
-    path('profile/', views.proofile_view, name = 'profile'),
+    path('profile/', views.profile_view, name = 'profile'),
     path('/posts/', views.PostListView.as_view(), name = 'post_list'),
     path('post/new/', views.PostCreateView.as_view(), name = 'post_create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name = 'post_detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name = 'post_edit'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post_delete')
-
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post_delete'),
+    path('/comment/<int:post_id>/comments/new/', views.CommentListView.as_view(), name = 'post-comments'),
+    path('/comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name = 'comment_update'),
+    path('/comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name = 'comment_delete'),
 }
