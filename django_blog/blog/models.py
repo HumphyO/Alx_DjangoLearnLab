2a72480f09e4b5b6a928e5c286f2b32b0385cfc1
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-07 22:23:56 +0300

    Added a tag model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,18 +1,51 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser, UserManager
+from django.conf import settings
 
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
 
-class User(models.Model):
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
-    bio = models.TextField(max_length=270, blank=True)
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class CustomUserManager(UserManager):
+    def create_user(self, email = ..., password = ..., **extra_fields):
+        if not email:
+            raise ValueError("Email field required")
+        user = self.create_user(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password)
+        user.is_staff= True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+
+class CustomUser(AbstractUser):
+    username= models.CharField(unique=True, max_length=130)
+    email = None
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    bio = models.TextField()
+    picture = models.URLField(max_length=180, blank=True)
 
 
 # Comment model for users to read and authenticated users to post edit and delete comments
@@ -22,6 +55,9 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+
+
 
 
 
