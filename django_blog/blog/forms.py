fd0d5a61678f0b78f69aa7033ec424aa9b55e73e
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-07 22:22:19 +0300

    Added tags to PostForm

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
+from taggit.forms import TagField
 
 #Create user form
 class UserCreationForm(UserCreationForm):
@@ -19,6 +20,8 @@ class UserCreationForm(UserCreationForm):
             return user
 
 class PostForm(forms.ModelForm):
+    tags = TagField(required=False)
+    
     class Meta:
         models = Post
         fields = ['title', 'content']
