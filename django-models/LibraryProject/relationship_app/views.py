7a5681931ebbe13b6d0fd8f8738b2290a27c7186
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-09 20:46:38 +0300

    Modified error to add missing statement

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
-from .models import Book
+from .models import Book, user
 from .models import Library
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
-from django.contrib.auth.decorators import user_passes_test, login_required 
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import permission_required, user_passes_test
 
 
 #Create Views here
+from.models import Book, Library
+# Access control
+def is_admin(user):
+    """Checks if user is an admin"""
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+
+
+
 #Function-based view
 @login_required
 def list_books(request):
@@ -71,30 +80,30 @@ def register_view(request):
     return render(request, 'relationship_app/register.html', {'form': form})
 
 
-def is_admin(user):
-    return user.is_authenticated and user.role == 'Admin'
+# def is_admin(user):
+#     return user.is_authenticated and user.userprofile.role == 'Admin'
 
-#Admin_view
-@user_passes_test(is_admin, login_url='/login/')
-def Admin_view(request):
-    return render(request, 'admin_view.html', {'role': 'Admin'})
+# #Admin_view
+# @user_passes_test(is_admin)
+# def admin_view(request):
+#     return render(request, 'admin_view.html', {'role': 'Admin'})
 
 
 
 def is_librarian(user):
-    return user.is_authenticated and user.role == 'Librarian'
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
 
 #Librarian_view
-def Librarian_view(request):
+def librarian_view(request):
     return render(request, 'librarian_view.html', {'role': 'Librarian'})
 
 def is_member(user):
-    return user.is_authenticated and user.role == 'Member'
+    return user.is_authenticated and user.userprofile.role == 'Member'
 
 #Member_view
-def Member_view(request):
+def member_view(request):
     return render(request, 'member_view.html', {'role': 'Member'})
 
-admin_view = user_passes_test(Admin_view)
-librarian_view = user_passes_test(Librarian_view)
-member_view = user_passes_test(Member_view)
+admin_view = user_passes_test(admin_view)
+librarian_view = user_passes_test(librarian_view)
+member_view = user_passes_test(member_view)
