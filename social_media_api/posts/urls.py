234150a479c71179585ffc21c9f4e062cc9ad22b
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-12 22:09:53 +0300

    Added router and url patterns to the file

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
 
+router = DefaultRouter()
+
+# Register PostViewSet and CommentViewSet 
+router.register(r'posts', PostViewSet, basename= 'post')
+router.register(r'comments', CommentViewSet, basename= 'comment')
 urlpatterns = [
-    path('')
+    path('api/', include ('posts.urls')),
 ]
