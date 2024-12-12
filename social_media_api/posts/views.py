2b9063b419a9d93b082d400a5b1cc17eaca3b7b4
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-12 22:11:01 +0300

    Modidfied the views.py to include CRUD operations

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -8,13 +8,19 @@ from rest_framework import permissions, viewsets
 # Create your views here.
 
 
-class PosViewSet(viewsets.ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        user =self.request.user
+        if user.is_staff:
+            return Post.objects.all()
+        return Post.objects.filter(author=user)
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
@@ -23,4 +29,20 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        user =self.request.user
+        return Comment.objects.filter(author=user)
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author != request.user:
+            return Response({'message': 'you are only allowed to edit your comments.'}, status=403)
+        return super().update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author != request.user:
+            return Response({'message': 'you are only allowed to delete your comments.'}, status=403)
+        return super().destroy(request, *args, **kwargs)
 
