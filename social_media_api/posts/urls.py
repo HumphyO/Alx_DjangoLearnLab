bb8072ac7cb2513c1827c200fb6c4adfc2a29c1f
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-15 16:28:03 +0300

    Added url patterns for like ann unlike

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, like_post, unlike_post
 
 router = DefaultRouter()
 
 # Register PostViewSet and CommentViewSet 
 router.register(r'posts', PostViewSet, basename= 'post')
 router.register(r'comments', CommentViewSet, basename= 'comment')
+
 urlpatterns = [
     path('api/', include ('posts.urls')),
     path('feed/', PostViewSet.as_view(), name='post_feed'),
+    path('/posts/<int:pk>'),
+    path('/posts/<int:pk>/like/', like_post.as_view(), name='like_post'),
+    path('/posts/<int:pk>/unlike/', unlike_post.as_view(), name='unlike_post'),
 ]
