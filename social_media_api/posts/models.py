df99afb00c0294c154d68c4f4a6d045b276d3356
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-12-15 14:56:49 +0300

    Added a like moel

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
@@ -34,4 +35,12 @@ class Like(models.Model):
         return f'{self.user} likes {self.post}'
 
 
+# class Notification(models.Model):
+#     recipient = models.ForeignKey(User, on_delete=models.CASCADE)
+#     actor = models.ForeignKey(User, on_delete=models.CASCADE)
+#     verb = models.CharField(max_length=300)
+#     target = GenericForeignKey('target_content_type' , 'target_id')
+#     timestamp = models.DateTimeField(auto_now_add=True)
 
+#     def __str__(self):
+#         return f'{self.actor} {self.verb} {self.target}'
