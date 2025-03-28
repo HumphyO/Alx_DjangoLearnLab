71d24227b0bb2563bd3f2a93ea1f6402a27561de
Humphrey Osoro <humphreymwebi18@gmail.com>
2025-03-28 18:01:00 +0300

    Added an endpoint for the movie recommendation

--- Movies_Review_api/movies/urls.py
+++ Movies_Review_api/movies/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ReviewList, ReviewDetail, UserList, UserDetail
+from .views import ReviewList, ReviewDetail, UserList, UserDetail,MovieRecommendationview
 
 
 
 urlpatterns = [
-    path('users/', UserList.as_view()),
+    path('users/', UserList.as_view(), name=''),
     path('users/<int:pk>/', UserDetail.as_view()),
     path('reviews/', ReviewList.as_view()),
-    path('reviews/<int:pk>/', ReviewDetail.as_view())
+    path('reviews/<int:pk>/', ReviewDetail.as_view()),
+    path('recommendations', MovieRecommendationview.as_view(), name = "movie-recommendations"),
 ]
